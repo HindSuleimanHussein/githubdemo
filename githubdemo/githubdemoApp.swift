c13c2107d08aa0b397fb7a8e78e441f02024b8b5
Hind Suleiman Hussein <hind.suleimanh@gmail.com>
2023-07-19 12:38:59 +0300

    Initial Commit

---
+++ githubdemo/githubdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubdemoApp.swift
+//  githubdemo
+//
+//  Created by Foothill on 19/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct githubdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
